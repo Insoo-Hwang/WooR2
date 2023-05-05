51d71eb00768ae76f4bc2443a1748a36f1f71c4a
ms <pms981125@naver.com>
2023-05-05 22:02:54 +0900

    UI 2차

--- app/src/main/java/com/example/woor2/SearchFragment.kt
+++ app/src/main/java/com/example/woor2/SearchFragment.kt
@@ -2,11 +2,13 @@ package com.example.woor2
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -31,6 +33,11 @@ class SearchFragment: Fragment()  {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
         recyclerView.setHasFixedSize(true)
+
+        val icon = context?.let { ContextCompat.getDrawable(it, R.drawable.search) }
+        context?.let { ContextCompat.getColor(it, R.color.A) }
+            ?.let { icon?.setColorFilter(it, PorterDuff.Mode.SRC_ATOP) }
+        binding.SearchingButton.setImageDrawable(icon)
 
         binding.SearchingButton.setOnClickListener{
             viewModel.deleteAll()
